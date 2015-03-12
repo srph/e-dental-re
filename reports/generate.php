58b0f06dbef8bbf7f0ef9b14a95d5292d27ff6ab
srph <seraphipod@gmail.com>
2015-03-13 03:33:22 +0800

    Added pdf generation and view.

--- reports/generate.php
+++ reports/generate.php
@@ -5,7 +5,7 @@ $middleware->run('admin');
 // var_dump((new Record)->find(1)->created_at->diffForHumans());
 
 $type = isset($_GET['type']) ? $_GET['type'] : 'records';
-$view = isset($_GET['view']) ? $_GET['view'] : true;
+$download = isset($_GET['download']) ? $_GET['download'] : false;
 
 ob_start();
 require_once(__DIR__ . "/templates/{$type}.php");
@@ -13,4 +13,4 @@ $html = ob_get_clean();
 
 $pdf->load_html($html);
 $pdf->render();
-$pdf->stream("report-{$type}.pdf", ['Attachment' => $view]);
+$pdf->stream("report-{$type}.pdf", ['Attachment' => $download]);
--- reports/index.php
+++ reports/index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../config.php';
-$middleware->run('auth');
+$middleware->run('admin');
 ?>
  
 <!DOCTYPE html>
@@ -21,9 +21,47 @@ $middleware->run('auth');
 		<div class="row">
 			<?php require __DIR__ . '/../inc/sidebar.php'; ?>
 			<div class="col-md-9">
-				<h1> Reports </h1>
+				<h1> Generate Reports </h1>
+				<hr>
+
+				<div class="form-group">
+					<label>
+						Download
+						<input type="checkbox" name="download">
+					</label>
+				</div>
+				<div class="btn-group">
+					<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+						Generate&nbsp;
+						<span class="caret"></span>
+					</button>
+
+					<ul class="dropdown-menu" role="menu">
+						<li> <a href="/reports/generate.php?type=records" target="_blank" data-dl><i class="glyphicon glyphicon-folder-open"></i> Records </a> </li>
+						<li> <a href="/reports/generate.php?type=schedules" target="_blank" data-dl><i class="glyphicon glyphicon-calendar"></i> Schedules </a> </li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
+
+	<script src="/inc/jquery.js"></script>
+	<script src="/inc/bootstrap.js"></script>
+	<script>
+	  $(function() {
+	  	var host = window.location.host;
+	  	
+	  	$('[data-dl]').on('click', function(evt) {
+	  		evt.preventDefault();
+	  		evt.stopPropagation && evt.stopPropagation();
+
+	  		var $download = $('input[name=download]');
+	  		var $this = $(this);
+	  		var attr = $this.attr('href');
+	  	
+	  		window.open('//' + host + attr + ($download.is(':checked') ? '&download=1' : ''));
+	  	});
+	  });
+	</script>
 </body>
 </html>
